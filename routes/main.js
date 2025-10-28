bfd4d716e4a7ac0bb91d8b3fb65099a4088731a4
Roan Jones <honestly2978@gmail.com>
2025-10-28 14:18:18 +0000

    Added registration form

--- routes/main.js
+++ routes/main.js
@@ -20,5 +20,20 @@ router.get("/search", (req, res) => {
     res.render("search.ejs", shopData);
 });
 
+router.get('/search_result', function (req, res) {
+    // TODO: search in the database
+    res.send("You searched for " + req.query.search_text + " in " + req.query.category);
+ });
+
+
+ router.get("/register", (req,res) => {
+    res.render("register.ejs",  shopData); 
+}); 
+ 
+router.post("/registered", (req,res) => { 
+  res.send(' Hello '+ req.body.first + ' '+ req.body.last + ' you are now registered! We will send an email to you at ' + req.body.email);    
+});
+
+
 // Export the router object so index.js can access it
 module.exports = router;
